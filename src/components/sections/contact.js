a247da446e740f1ab7473ee04d5350a47fd98828
Aizer Ravelo <aizer@groundline.ph>
2021-09-27 11:09:38 +0800

    fix: contact us section

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -7,6 +7,11 @@ import { usePrefersReducedMotion } from '@hooks';
 const StyledContactSection = styled.section`
   margin: 0 auto 100px;
   text-align: left;
+  color: var(--dark-navy);
+
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  grid-gap: 50px;
 
   @media (max-width: 768px) {
     margin: 0 auto 50px;
@@ -15,7 +20,7 @@ const StyledContactSection = styled.section`
   .overline {
     display: block;
     margin-bottom: 20px;
-    color: var(--white);
+    color: var(--dark-navy);
     font-size: var(--fz-sm);    
     font-weight: bold;
     text-transform: uppercase;
@@ -33,11 +38,45 @@ const StyledContactSection = styled.section`
 
   .title {
     font-size: clamp(40px, 5vw, 60px);
+    color: var(--dark-slate);
   }
 
   .email-link {
     ${({ theme }) => theme.mixins.bigButton};
     margin-top: 50px;
+  }
+
+  .contact-form {
+    input, textarea {
+      width: 100%;
+      padding: 1rem;
+      outline: 0;
+      margin-bottom: 0.5rem;
+      font-size: 1rem;
+      font-family: inherit;
+      display: block;
+      border-width: 1px;
+      border-radius: 0.25rem;
+      --border-opacity: 1;
+      box-sizing: border-box;
+      -webkit-appearance: none;
+      touch-action: manipulation;
+      border: 1px solid #cbd5e0;
+    }
+
+    button {
+      background-color: #1890ff;
+      -webkit-appearance: button;
+      --text-opacity: 1;
+      color: rgba(255,255,255,var(--text-opacity));
+      padding-left: 2rem;
+      padding-right: 2rem;
+      padding-top: .5rem;
+      padding-bottom: .5rem;
+      font-size: 1rem;
+      border-width: 1px;
+      border-radius: .25rem;
+    }
   }
 `;
 
@@ -54,20 +93,52 @@ const Contact = () => {
   }, []);
 
   return (
-    <StyledContactSection id="contact" ref={revealContainer}>
-      <h2 className="numbered-heading overline">Contact Us</h2>
+    <div style={{backgroundColor: '#FFFFFF'}}>
+      <StyledContactSection id="contact" ref={revealContainer}>
 
-      <h2 className="title">Get In Touch</h2>
+        <div>
+          <h4 className="numbered-heading overline">Contact Us</h4>
+          <h2 className="title">Get In Touch</h2>
 
-      <p>
-      We are happy to hear from you. To know more about our product and how we can help improve your business workflow, 
-      send your message using the form and we will get in touch with you as soon as possible.
-      </p>
+          <p>
+          We are happy to hear from you. To know more about our work and how we can help you, 
+          send your message using the form and we will get in touch with you as soon as possible.
+          </p>
+        </div>
 
-      <a className="email-link" href={`mailto:${email}`}>
-        Say Hello
-      </a>
-    </StyledContactSection>
+        <form action="/#contact" name="contact" method="POST" className="contact-form" data-netlify-honeypot="bot-field" data-netlify="true" id="contact-form">
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
+            <input 
+              type="text" 
+              name="name" 
+              id="contact-form-name" 
+              aria-labelledby="contact-form-name-label" 
+              placeholder="Name" 
+            />
+            <input 
+              type="email" 
+              name="email" 
+              placeholder="Email Address" 
+              id="contact-form-email" 
+              aria-labelledby="contact-form-email-label" 
+            />
+            <textarea
+                name="message"
+                id="contact-form-message"
+                aria-labelledby="contact-form-message-label"
+                rows="4"
+                placeholder="Message"
+            />
+            <button 
+              type="submit" 
+              style={{backgroundColor: "#1890ff"}}
+              > Send
+            </button>
+        </form>
+
+      </StyledContactSection>
+    </div>
   );
 };
 
