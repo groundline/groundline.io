af1e99fa1bab331417bd3b93b453078775817dcb
Aizer Ravelo <aizer@groundline.ph>
2021-09-30 10:09:03 +0800

    fix: metadata title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Groundline',
+    title: 'Groundline | Geospatial monitoring and analysis',
     description: 'Geospatial monitoring and analysis',
     siteUrl: 'https://groundline.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
